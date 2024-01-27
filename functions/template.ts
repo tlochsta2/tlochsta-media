7803414f20d374846075c0943a00534e247de9df
tlochsta <81081702+TlochstaDev@users.noreply.github.com>
2024-01-27 17:24:12 -0500

    Update template.ts

--- functions/template.ts
+++ functions/template.ts
@@ -6,56 +6,162 @@ export function getTemplate({
   withError: boolean;
 }): string {
   return `
-  <!doctype html>
-  <html lang="en" data-theme="dark">
+  <html lang="en" data-bs-theme="dark"><head>
 
-    <head>
-      <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <title>Password Protected Site</title>
-      <meta name="description" content="This site is password protected.">
-      <link rel="shortcut icon" href="https://picocss.com/favicon.ico">
+    <meta charset="utf-8">
+    
+    
+    
+    
+    <title>Authenticate</title>
 
-      <link rel="stylesheet" href="https://unpkg.com/@picocss/pico@latest/css/pico.min.css">
+    
 
-      <style>
-        body > main {
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          min-height: calc(100vh - 7rem);
-          padding: 1rem 0;
-          max-width: 600px;
+    
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3">
+
+<link rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" href="https://getbootstrap.com/docs/5.3/dist/css/bootstrap.min.css">
+
+    
+
+
+
+
+
+
+
+
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
         }
+      }
 
-        .error {
-          background: white;
-          border-radius: 10px;
-          color: var(--del-color);
-          padding: 0.5em 1em;
-        }
+      .b-example-divider {
+        width: 100%;
+        height: 3rem;
+        background-color: rgba(0, 0, 0, .1);
+        border: solid rgba(0, 0, 0, .15);
+        border-width: 1px 0;
+        box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
+      }
 
-        h2 { color: var(--color-h2); }
-      </style>
-    </head>
+      .b-example-vr {
+        flex-shrink: 0;
+        width: 1.5rem;
+        height: 100vh;
+      }
 
-    <body>
-      <main>
-        <article>
-          <hgroup>
-            <h1>Password</h1>
-            <h2>Please enter your password for this site.</h2>
-          </hgroup>
-          ${withError ? `<p class="error">Incorrect password, please try again.</p>` : ''}
-          <form method="post" action="/cfp_login">
-            <input type="hidden" name="redirect" value="${redirectPath}" />
-            <input type="password" name="password" placeholder="Password" aria-label="Password" autocomplete="current-password" required autofocus>
-            <button type="submit" class="contrast">Login</button>
-          </form>
-        </article>
-      </main>
-    </body>
+      .bi {
+        vertical-align: -.125em;
+        fill: currentColor;
+      }
 
-  </html>
+      .nav-scroller {
+        position: relative;
+        z-index: 2;
+        height: 2.75rem;
+        overflow-y: hidden;
+      }
+
+      .nav-scroller .nav {
+        display: flex;
+        flex-wrap: nowrap;
+        padding-bottom: 1rem;
+        margin-top: -1px;
+        overflow-x: auto;
+        text-align: center;
+        white-space: nowrap;
+        -webkit-overflow-scrolling: touch;
+      }
+
+      .btn-bd-primary {
+        --bd-violet-bg: #712cf9;
+        --bd-violet-rgb: 112.520718, 44.062154, 249.437846;
+
+        --bs-btn-font-weight: 600;
+        --bs-btn-color: var(--bs-white);
+        --bs-btn-bg: var(--bd-violet-bg);
+        --bs-btn-border-color: var(--bd-violet-bg);
+        --bs-btn-hover-color: var(--bs-white);
+        --bs-btn-hover-bg: #6528e0;
+        --bs-btn-hover-border-color: #6528e0;
+        --bs-btn-focus-shadow-rgb: var(--bd-violet-rgb);
+        --bs-btn-active-color: var(--bs-btn-hover-color);
+        --bs-btn-active-bg: #5a23c8;
+        --bs-btn-active-border-color: #5a23c8;
+      }
+
+      .bd-mode-toggle {
+        z-index: 1500;
+      }
+
+      .bd-mode-toggle .dropdown-menu .active .bi {
+        display: block !important;
+      }
+    html,
+body {
+  height: 100%;
+}
+
+.form-signin {
+  max-width: 330px;
+  padding: 1rem;
+}
+
+.form-signin .form-floating:focus-within {
+  z-index: 2;
+}
+
+.form-signin input {
+margin-bottom: 10px;
+}
+
+</style>
+
+    
+    
+    
+  </head>
+  <body class="d-flex align-items-center py-4 bg-body-tertiary">
+    
+
+    
+
+    
+<main class="form-signin w-100 m-auto">
+  <form method="post" action="/cfp_login">
+    
+    <h1 class="h3 mb-3 fw-normal">tlochsta media</h1>
+
+    
+    <div class="form-floating">
+<input type="hidden" name="redirect" style="display:none;" value="${redirectPath}">
+      <input type="password" class="form-control" autocomplete="current-password" autofocus="" placeholder="Password" required="" id="password">
+      <label for="password">Site Password</label>
+    </div>
+
+    
+    <button type="submit" class="btn btn-dark w-100 py-2">Sign in</button>
+    <p class="mt-2 mb-3 text-body-secondary">© 2024 tlch.xyz</p>
+${withError ? `<p class="mt-1 mb-3 text-danger">Incorrect password, please try again.</p>` : ''} 
+  </form>
+</main>
+<script integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous" src="https://getbootstrap.com/docs/5.3/dist/js/bootstrap.bundle.min.js"></script>
+
+    
+
+</body></html>
   `;
 }
