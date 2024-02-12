9e42781989086e18ef0a9420224b102a5b3012e2
tlochsta <81081702+TlochstaDev@users.noreply.github.com>
2024-02-11 20:47:39 -0500

    Update template.ts

--- functions/template.ts
+++ functions/template.ts
@@ -6,162 +6,41 @@ export function getTemplate({
   withError: boolean;
 }): string {
   return `
-  <html lang="en" data-bs-theme="dark"><head>
+  <!DOCTYPE html>
+<html lang="en" data-bs-theme="dark">
 
+<head>
     <meta charset="utf-8">
-    
-    
-    
-    
-    <title>Authenticate</title>
+    <title>tlochsta media</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
+<meta name="msapplication-TileColor" content="#603cba">
+<meta name="theme-color" content="#ffffff">
+</head>
 
-    
+<body class="bg-gray-900 text-white flex items-center justify-center h-screen">
+    <main class="max-w-md w-full bg-purple-800 p-8 rounded-lg shadow-lg">
+        <form method="post" action="/cfp_login" class="space-y-6">
+            <h1 class="text-3xl font-semibold text-center mb-6"><img src="https://github.com/tlochsta-media/static-edition/blob/main/android-chrome-384x384.png?raw=true" style="max-height: 120px;display:inline" ></h1>
+            <div class="relative">
+                <input type="hidden" name="redirect" value="${redirectPath}">
+                <input type="password" name="password" autocomplete="current-password" autofocus
+                    class="w-full px-4 py-2 rounded-lg bg-purple-900 border border-gray-700 focus:outline-none border-purple-500"
+                    placeholder="Password" required id="password">
+            </div>
+            <button type="submit"
+                class="w-full py-2 bg-purple-600 text-white font-semibold rounded-lg shadow-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2">
+                Sign in
+            </button>
+            ${withError ? `<p class="mt-1 mb-3 text-center text-white">Incorrect password, please try again.</p>` : ''}
+        </form>
+    </main>
+</body>
 
-    
+</html>
 
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3">
-
-<link rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" href="https://getbootstrap.com/docs/5.3/dist/css/bootstrap.min.css">
-
-    
-
-
-
-
-
-
-
-
-
-    <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
-      }
-
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
-
-      .b-example-divider {
-        width: 100%;
-        height: 3rem;
-        background-color: rgba(0, 0, 0, .1);
-        border: solid rgba(0, 0, 0, .15);
-        border-width: 1px 0;
-        box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
-      }
-
-      .b-example-vr {
-        flex-shrink: 0;
-        width: 1.5rem;
-        height: 100vh;
-      }
-
-      .bi {
-        vertical-align: -.125em;
-        fill: currentColor;
-      }
-
-      .nav-scroller {
-        position: relative;
-        z-index: 2;
-        height: 2.75rem;
-        overflow-y: hidden;
-      }
-
-      .nav-scroller .nav {
-        display: flex;
-        flex-wrap: nowrap;
-        padding-bottom: 1rem;
-        margin-top: -1px;
-        overflow-x: auto;
-        text-align: center;
-        white-space: nowrap;
-        -webkit-overflow-scrolling: touch;
-      }
-
-      .btn-bd-primary {
-        --bd-violet-bg: #712cf9;
-        --bd-violet-rgb: 112.520718, 44.062154, 249.437846;
-
-        --bs-btn-font-weight: 600;
-        --bs-btn-color: var(--bs-white);
-        --bs-btn-bg: var(--bd-violet-bg);
-        --bs-btn-border-color: var(--bd-violet-bg);
-        --bs-btn-hover-color: var(--bs-white);
-        --bs-btn-hover-bg: #6528e0;
-        --bs-btn-hover-border-color: #6528e0;
-        --bs-btn-focus-shadow-rgb: var(--bd-violet-rgb);
-        --bs-btn-active-color: var(--bs-btn-hover-color);
-        --bs-btn-active-bg: #5a23c8;
-        --bs-btn-active-border-color: #5a23c8;
-      }
-
-      .bd-mode-toggle {
-        z-index: 1500;
-      }
-
-      .bd-mode-toggle .dropdown-menu .active .bi {
-        display: block !important;
-      }
-    html,
-body {
-  height: 100%;
-}
-
-.form-signin {
-  max-width: 330px;
-  padding: 1rem;
-}
-
-.form-signin .form-floating:focus-within {
-  z-index: 2;
-}
-
-.form-signin input {
-margin-bottom: 10px;
-}
-
-</style>
-
-    
-    
-    
-  </head>
-  <body class="d-flex align-items-center py-4 bg-body-tertiary">
-    
-
-    
-
-    
-<main class="form-signin w-100 m-auto">
-  <form method="post" action="/cfp_login">
-    
-    <h1 class="h3 mb-3 fw-normal">tlochsta media</h1>
-
-    
-    <div class="form-floating">
-<input type="hidden" name="redirect" style="display:none;" value="${redirectPath}">
-      <input type="password" name="password" class="form-control" autocomplete="current-password" autofocus="" placeholder="Password" required="" id="password">
-      <label for="password">Site Password</label>
-    </div>
-
-    
-    <button type="submit" class="btn btn-dark w-100 py-2">Sign in</button>
-    <p class="mt-2 mb-3 text-body-secondary">© 2024 tlch.xyz</p>
-${withError ? `<p class="mt-1 mb-3 text-danger">Incorrect password, please try again.</p>` : ''} 
-  </form>
-</main>
-<script integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous" src="https://getbootstrap.com/docs/5.3/dist/js/bootstrap.bundle.min.js"></script>
-
-    
-
-</body></html>
   `;
 }
